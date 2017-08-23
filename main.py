5d2785d8be6b2e4f568014b1cdb2febe8fce62e8
Daniel Santos <tnik@localhost.localdomain>
2017-08-23 18:55:29 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+import redis
+
+app = Flask(__name__)
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@app.route("/")
+def hello():
+    page = "<h1>Hello Flask</h1>"
+    keys = r.keys()
+    for key in keys:
+        page += "<p style='color:red'>" + key + "-" + r.get(key) + "</p>"
+    return page
+
+@app.route("/add/<text>/<value>")
+def add(text, value):
+    r.set(text, value)
+    return text+' has been received'
